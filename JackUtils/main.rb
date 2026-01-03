6d4be14c51e58738ef07b02c57976746dd5e07e1
tenrev <92268421+tenrev2009@users.noreply.github.com>
2026-01-03 18:05:03 +0100

    Add files via upload

--- JackUtils/main.rb
+++ JackUtils/main.rb
@@ -2,20 +2,26 @@
 require 'sketchup.rb'
 
 module JackUtils
+  # Chemin racine du plugin
   PATH_ROOT = File.dirname(__FILE__)
   PATH_ICONS = File.join(PATH_ROOT, 'assets', 'icons')
   
-  # Chargement de tous les outils
-  Dir.glob(File.join(PATH_ROOT, 'tools', '*.rb')).each { |f| require f }
+  # Chargement explicite pour éviter les oublis
+  Dir.glob(File.join(PATH_ROOT, 'tools', '*.rb')).each do |file|
+    begin
+      require file
+    rescue => e
+      UI.messagebox("Erreur chargement #{File.basename(file)}: #{e.message}")
+    end
+  end
 
   def self.create_cmd(title, icon_name, tooltip, &block)
     cmd = UI::Command.new(title) { block.call }
-    # On cherche l'icone, sinon on met une icone par defaut ou nil
     icon_path = File.join(PATH_ICONS, icon_name)
+    
     if File.exist?(icon_path)
-      cmd.small_icon = cmd.large_icon = icon_path
-    else
-      puts "JackUtils: Icône manquante -> #{icon_name}"
+      cmd.small_icon = icon_path
+      cmd.large_icon = icon_path
     end
     cmd.tooltip = tooltip
     cmd.status_bar_text = tooltip
@@ -25,83 +31,61 @@ module JackUtils
   unless file_loaded?(__FILE__)
     tb = UI::Toolbar.new("Jack Tools")
 
-    # --- 1. MODÉLISATION ---
+    # --- MODELISATION ---
+    if defined?(JackUtils::Unique)
+      tb.add_item(create_cmd("Rendre Unique", "unique.png", "Rendre Unique (Deep)") { JackUtils::Unique.make_selection_unique })
+    end
     
-    # Rendre Unique (Basé sur unique.rb)
-    cmd = create_cmd("Rendre Unique", "unique.png", "Rendre sélection unique (récursif)") { JackUtils::Unique.make_selection_unique }
-    tb.add_item(cmd)
+    if defined?(JackUtils::SelectSimilar)
+      tb.add_item(create_cmd("Select Similar", "c_similar_small.png", "Sélectionner Similaires") { JackUtils::SelectSimilar.select_similar })
+    end
 
-    # Sélection Similaire
-    cmd = create_cmd("Select Similar", "c_similar_small.png", "Sélectionner composants similaires") { JackUtils::SelectSimilar.select_similar }
-    tb.add_item(cmd)
-
-    # Match Component (Refaire Compo)
-    cmd = create_cmd("Refaire Compo", "compo.png", "Remplacer définition (Match)") { JackUtils::MatchCompo.start_tool }
-    tb.add_item(cmd)
+    if defined?(JackUtils::MatchCompo)
+      tb.add_item(create_cmd("Refaire Compo", "compo.png", "Remplacer (Match)") { JackUtils::MatchCompo.start_tool })
+    end
     
     tb.add_separator
 
-    # --- 2. VISIBILITÉ ---
+    # --- VISIBILITE ---
+    if defined?(JackUtils::Tags)
+      tb.add_item(create_cmd("Isoler Balises", "balise_jack.png", "Isoler Balises") { JackUtils::Tags.masquer_autres })
+      tb.add_item(create_cmd("Restaurer Balises", "balise_jack1.png", "Restaurer Balises") { JackUtils::Tags.restaurer })
+    end
 
-    # Isoler Balises
-    cmd = create_cmd("Isoler Balises", "balise_jack.png", "Masquer les autres balises") { JackUtils::Tags.masquer_autres }
-    tb.add_item(cmd)
-
-    # Restaurer Balises
-    cmd = create_cmd("Restaurer Balises", "balise_jack1.png", "Restaurer l'état des balises") { JackUtils::Tags.restaurer }
-    tb.add_item(cmd)
-
-    # Supprimer Sous-composants Masqués
-    cmd = create_cmd("Purge Masqués", "remove_hidden_small.png", "Supprimer sous-composants masqués") { JackUtils::CleanHidden.remove }
-    tb.add_item(cmd)
+    if defined?(JackUtils::CleanHidden)
+      tb.add_item(create_cmd("Purge Masqués", "remove_hidden_small.png", "Purge Masqués") { JackUtils::CleanHidden.remove })
+    end
 
     tb.add_separator
 
-    # --- 3. ATTRIBUTS & DATA (DC) ---
+    # --- ATTRIBUTS DC ---
+    if defined?(JackUtils::CleanDC)
+      tb.add_item(create_cmd("Supprimer DC", "remove_dynamic_small.png", "Supprimer DC") { JackUtils::CleanDC.remove })
+    end
 
-    # Supprimer Attributs Dynamiques
-    cmd = create_cmd("Supprimer DC", "remove_dynamic_small.png", "Supprimer attributs dynamiques") { JackUtils::CleanDC.remove }
-    tb.add_item(cmd)
-
-    # Data CSV - Export
-    cmd = create_cmd("Data Export", "export.png", "Exporter attributs CSV") { JackUtils::DataCSV.export }
-    tb.add_item(cmd)
-
-    # Data CSV - Import
-    cmd = create_cmd("Data Import", "import.png", "Importer attributs CSV") { JackUtils::DataCSV.import }
-    tb.add_item(cmd)
-
-    # Data CSV - Redraw
-    cmd = create_cmd("Forcer Redraw", "redessiner.png", "Redessiner DC Sélection") { JackUtils::DataCSV.redraw }
-    tb.add_item(cmd)
+    if defined?(JackUtils::DataCSV)
+      tb.add_item(create_cmd("Export CSV", "export.png", "Export Attributs") { JackUtils::DataCSV.export })
+      tb.add_item(create_cmd("Import CSV", "import.png", "Import Attributs") { JackUtils::DataCSV.import })
+      tb.add_item(create_cmd("Redraw", "redessiner.png", "Force Redraw") { JackUtils::DataCSV.redraw })
+    end
 
     tb.add_separator
 
-    # --- 4. FICHIERS (IO) ---
-
-    # Export Composants (.skp)
-    cmd = create_cmd("Export SKP", "exportc.png", "Exporter composants en .skp") { JackUtils::IOSkp.export_components }
-    tb.add_item(cmd)
-
-    # Import Dossier
-    cmd = create_cmd("Import Dossier", "library_small.png", "Importer dossier de .skp") { JackUtils::IOSkp.import_folder }
-    tb.add_item(cmd)
-    
-    # Import Armoire (Spécifique)
-    cmd = create_cmd("Armoire", "armoire.png", "Importer Armoire") { JackUtils::IOSkp.import_armoire }
-    tb.add_item(cmd)
+    # --- FICHIERS ---
+    if defined?(JackUtils::IOSkp)
+      tb.add_item(create_cmd("Export SKP", "exportc.png", "Export Composants") { JackUtils::IOSkp.export_components })
+      tb.add_item(create_cmd("Import Dossier", "library_small.png", "Import Dossier") { JackUtils::IOSkp.import_folder })
+      tb.add_item(create_cmd("Armoire", "armoire.png", "Armoire") { JackUtils::IOSkp.import_armoire })
+    end
 
     tb.add_separator
 
-    # --- 5. SYSTÈME ---
-    
-    # Purge Recent
-    cmd = create_cmd("Purge Récent", "purge.png", "Purger fichiers récents") { JackUtils::System.purge_recent }
-    tb.add_item(cmd)
-    
-    # Hot Reload (Dev)
-    cmd = create_cmd("Reload Plugins", "reload.png", "Recharger Plugins (Dev)") { JackUtils::System.hot_reload_ui }
-    tb.add_item(cmd)
+    # --- SYSTEME ---
+    # Ici on appelle directement les fonctions du module System
+    if defined?(JackUtils::System)
+      tb.add_item(create_cmd("Purge Récent", "purge.png", "Purger Fichiers Récents") { JackUtils::System.purge_recent })
+      tb.add_item(create_cmd("Reload Plugins", "reload.png", "Reload UI") { JackUtils::System.show_reload_dialog })
+    end
 
     tb.restore
     file_loaded(__FILE__)
